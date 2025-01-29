845fdd3c1b78dd33f3b4a65754cb95af159b33e0
Jorge <jorge.dominguez@n26.com>
2025-01-29 15:35:00 +0100

    Visual improvements applied to chart

--- composeApp/src/desktopMain/kotlin/org/n27/charts/common/composables/charts/PieChart.kt
+++ composeApp/src/desktopMain/kotlin/org/n27/charts/common/composables/charts/PieChart.kt
@@ -1,28 +1,33 @@
 package org.n27.charts.common.composables.charts
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
 
 @Composable
 fun PieChart(
     data: ImmutableList<Float>,
     colors: ImmutableList<Color>,
-    thickness: Float = 40f,
-    modifier: Modifier = Modifier.size(200.dp),
+    thicknessRatio: Float = 0.2f,
 ) {
     val total = data.sum()
     if (total == 0f) return
 
-    Canvas(modifier = modifier) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize(0.70f)
+            .aspectRatio(1f)
+    ) {
         var startAngle = 0f
+        val minSize = minOf(size.width, size.height)
+        val thickness = minSize * thicknessRatio
 
         data.forEachIndexed { index, value ->
             val sweepAngle = (value / total) * 360f
